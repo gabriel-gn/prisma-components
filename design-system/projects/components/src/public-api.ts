f0e3ca26a746ebb65cdd3bd7e4a3f5d87da45dc8
Gabriel Nogueira <gabrielgomesnogueira@gmail.com>
2021-04-22 23:28:05 -0300

    :wrench: exportando componente e módulo

--- design-system/projects/components/src/public-api.ts
+++ design-system/projects/components/src/public-api.ts
@@ -6,5 +6,12 @@
 export * from './lib/components.service';
 export * from './lib/components.component';
 export * from './lib/components.module';
+
 export * from './lib/button/button.component';
+export * from './lib/button/button.module';
+
 export * from './lib/draggable-list/draggable-list.component';
+export * from './lib/draggable-list/draggable-list.module';
+
+export * from './lib/table/table.component';
+export * from './lib/table/table.module';
