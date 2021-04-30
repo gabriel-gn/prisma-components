c7e7bd6c246ff92b5a6ac2ff5e15d544c160aff9
Gabriel Nogueira <gabrielgomesnogueira@gmail.com>
2021-04-30 15:24:19 -0300

    :wrench: Double label

--- design-system/stories/Table.stories.ts
+++ design-system/stories/Table.stories.ts
@@ -55,7 +55,7 @@ ExampleTable.args = {
   numberPrefix: 'R$',
   itemList: [
     {preview: 'https://pbs.twimg.com/profile_images/661223657528651776/94PccMFW.jpg',
-      name: 'Imagem1', price: 2.99, author: 'admin'},
+      name: ['Imagem1', 'Edição limitada'], price: 2.99, author: 'admin'},
     {preview: 'https://logz.io/wp-content/uploads/2017/03/open-source-bi-tools-1280x720.jpg',
       name: 'Imagem1', price: 2.99, author: 'admin22'},
   ],
