65b1bdf46705c2d8332cd7a58d805bfdae3d8409
Gabriel Nogueira <gabrielgomesnogueira@gmail.com>
2021-05-02 23:37:37 -0300

    :wrench: css and scss files in different import files .scss

--- design-system/.storybook/preview.js
+++ design-system/.storybook/preview.js
@@ -1,5 +1,6 @@
 import {setCompodocJson} from "@storybook/addon-docs/angular";
 import docJson from "../documentation.json";
+import '!style-loader!css-loader!sass-loader!./../projects/components/src/styles/base_css.scss';
 import '!style-loader!css-loader!sass-loader!./../projects/components/src/styles/base.scss';
 
 setCompodocJson(docJson);
