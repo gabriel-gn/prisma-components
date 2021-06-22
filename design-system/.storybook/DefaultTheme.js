c32959233cb7f6334713f6dc401102cb6d3145f9
Gabriel Nogueira <gabrielgomesnogueira@gmail.com>
2021-06-21 23:33:03 -0300

    :sparkles: Meu prisma logo

---
+++ design-system/.storybook/DefaultTheme.js
@@ -0,0 +1,32 @@
+import {create} from '@storybook/theming';
+
+export default create({
+
+  brandTitle: 'Meu Prisma',
+  brandUrl: 'https://meuprisma.com',
+  brandImage: 'logo.png',
+
+  base: 'light',
+
+  // colorPrimary: '#808080',
+  // colorSecondary: '#000000',
+  // appBg: '#705795',
+  // appContentBg: '#573877',
+  // appBorderColor: '#0000ff',
+  // appBorderRadius: '#800000',
+  // textColor: '#F3A772',
+  // textInverseColor: '#00ffff',
+  // textMutedColor: '#ff00ff',
+  // barTextColor: '#ff80ff',
+  // barSelectedColor: '#008000',
+  // barBg: '#000080',
+  // inputBg: '#ffffff',
+  // inputBorder: '#ffffff',
+  // inputTextColor: '#ffffff',
+  // inputBorderRadius: '#ffffff',
+  // gridCellSize: '#ffffff',
+
+  fontBase: '"Open Sans", sans-serif',
+  fontCode: 'monospace',
+
+});
