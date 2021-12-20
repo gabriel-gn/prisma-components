a0c9af0da4c0f9b4ce9da7c862f7671a6b99a648
Gabriel Nogueira <gabrielgomesnogueira@gmail.com>
2021-12-20 15:29:06 -0300

    :wrench: documentação pm-basic-card

--- design-system/projects/lib/src/components/cards/basic-card/basic-card.stories.ts
+++ design-system/projects/lib/src/components/cards/basic-card/basic-card.stories.ts
@@ -1,6 +1,7 @@
 import {BasicCardComponent} from './basic-card.component';
 import {Meta} from '@storybook/angular/types-6-0';
 import {Story} from '@storybook/angular';
+import {Sizes} from '../../../models/sizes';
 
 export default {
   title: 'Prisma/Stories/BasicCard',
@@ -11,12 +12,18 @@ export const Template: Story<BasicCardComponent> = (args) => ({
   props: args,
   template: `
     <pm-basic-card
+      [hoverable]="hoverable"
+      [borderRadius]="borderRadius"
+      [paddingClass]="paddingClass"
     >
     </pm-basic-card>
   `
 });
 
 export const defaultArgs = {
+  hoverable: false,
+  borderRadius: Sizes.md,
+  paddingClass: 'p-3'
 };
 
 export const DefaultBasicCard = Template.bind({});
