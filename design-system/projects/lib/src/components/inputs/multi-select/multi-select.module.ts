aa7251f61fadc75b90b18e14608286405a30b671
Gabriel Nogueira <gabrielgomesnogueira@gmail.com>
2021-12-27 00:32:27 -0300

    :wrench: ajuste na importacao de modulos

--- design-system/projects/lib/src/components/inputs/multi-select/multi-select.module.ts
+++ design-system/projects/lib/src/components/inputs/multi-select/multi-select.module.ts
@@ -2,10 +2,9 @@ import {NgModule} from '@angular/core';
 import {MultiSelectComponent} from './multi-select.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatInputModule} from '@angular/material/input';
+import {CommonModule} from '@angular/common';
 
 
 @NgModule({
@@ -16,8 +15,7 @@ import {MatInputModule} from '@angular/material/input';
     MultiSelectComponent
   ],
   imports: [
-    BrowserAnimationsModule,
-    BrowserModule,
+    CommonModule,
     FormsModule,
     ReactiveFormsModule,
     MatAutocompleteModule,
