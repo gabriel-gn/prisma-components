9e1dcf1d6d18afffe7445cfdf52ae09bb855840d
Gabriel Nogueira <gabrielgomesnogueira@gmail.com>
2022-09-24 17:55:27 -0300

    :wrench: ajuste para build

--- scripts/remove-styles-from-build.js
+++ scripts/remove-styles-from-build.js
@@ -5,5 +5,6 @@ fileContent = fs.readFileSync("angular.json");
 const angularObject = JSON.parse(fileContent.toString());
 console.log('Arquivo lido com sucesso');
 delete angularObject.projects.lib.architect.build.options['styles'];
+delete angularObject.projects.lib.architect.build.options['scripts'];
 fs.writeFileSync("angular.json", JSON.stringify(angularObject));
 console.log('Alteração no angular.json finalizada');
