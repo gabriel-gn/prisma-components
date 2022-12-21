e2f53de26d24c229f4d699986236598e998c897b
Gabriel Nogueira <gabrielgomesnogueira@gmail.com>
2022-12-21 16:52:24 -0300

    :wrench: checkpoint

---
+++ scripts/revert-git-changes.js
@@ -0,0 +1,12 @@
+const shell = require("shelljs");
+
+const filesToRevert = [
+  'angular.json',
+  'src/services/color-theme/color-theme.service.ts'
+]
+
+filesToRevert.forEach(filePath => {
+  shell.exec(`git reset HEAD ${filePath}`);
+  shell.exec(`git checkout -- ${filePath}`);
+})
+console.log(`Alteração nos arquivos ${filesToRevert} revertidos`);
