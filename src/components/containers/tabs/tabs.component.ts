de729e1df495f7b2b13dcd44c3e32a073fd2c1ae
Gabriel Nogueira <gabrielgomesnogueira@gmail.com>
2022-12-21 17:44:31 -0300

    :wrench: checkpoint

--- src/components/containers/tabs/tabs.component.ts
+++ src/components/containers/tabs/tabs.component.ts
@@ -1,20 +1,21 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'pm-tabs',
   templateUrl: './tabs.component.html',
   styleUrls: ['./tabs.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class TabsComponent {
 
   /**
    * Tab orientation
    */
-  @Input() orientation: 'horizontal' | 'vertical' = 'horizontal'
+  @Input() orientation: 'horizontal' | 'vertical' = 'horizontal';
   /**
    * Sticky to top or bottom
    */
-  @Input() sticky: boolean = true
+  @Input() sticky: boolean = true;
 
   constructor() {
   }
